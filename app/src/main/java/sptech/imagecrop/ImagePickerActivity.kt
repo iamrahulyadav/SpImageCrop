3d16de8c5b003318e9ad4ae75a23e77df80bbe8e
SIBAPRASAD <neosoft.sibaprasad@angelbroking.com>
2018-05-28 14:50:43 +0530

    First Code COmmit

---
+++ app/src/main/java/sptech/imagecrop/ImagePickerActivity.kt
@@ -0,0 +1,12 @@
+package sptech.imagecrop
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ImagePickerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_picker)
+    }
+}
